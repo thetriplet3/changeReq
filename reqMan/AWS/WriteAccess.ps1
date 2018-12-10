650576899a7e6a3e55b79057e3f5cbd3f510aacf
thetriplet3 <thetriplet3@gmail.com>
2018-12-11 00:50:52 +0530

    Fixed AWS Issue

---
+++ reqMan/AWS/WriteAccess.ps1
@@ -0,0 +1,7 @@
+﻿# # WriteAccess.ps1 # 
+$sharepath = "C:\inetpub\AspNetCoreWebApps\app\wwwroot"
+$Acl = Get-ACL 
+$SharePath 
+$AccessRule= New-Object System.Security.AccessControl.FileSystemAccessRule("DefaultAppPool","full","ContainerInherit,Objectinherit","none","Allow") 
+$Acl.AddAccessRule($AccessRule) 
+Set-Acl $SharePath $Acl
